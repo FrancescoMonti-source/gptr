effd2b56c2f2e450356658e17b4dc32aa611858a
Francesco Monti <58150610+FrancescoMonti-source@users.noreply.github.com>
2025-09-07 00:09:27 +0200

    Update helper-mock.R

--- tests/testthat/helper-mock.R
+++ tests/testthat/helper-mock.R
@@ -1,5 +1,11 @@
 local_gptr_mock <- function(..., .envir = parent.frame()) {
-    testthat::local_mocked_bindings(...,
-                                    .env = asNamespace("gptr"),
-                                    .local_envir = .envir)
+    fn <- testthat::local_mocked_bindings
+    args <- names(formals(fn))
+    if (".local_envir" %in% args) {
+        fn(..., .env = asNamespace("gptr"), .local_envir = .envir)
+    } else if (".local_env" %in% args) {
+        fn(..., .env = asNamespace("gptr"), .local_env = .envir)
+    } else {
+        fn(..., .env = asNamespace("gptr"), .envir = .envir)
+    }
 }
